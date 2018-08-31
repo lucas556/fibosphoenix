b179bf90cdddba31b2796152d26ae8236bcc591f
lucas556 <56923561@qq.com>
2018-08-31 21:15:49 +0800

    Update create_account.js

--- create_account.js
+++ create_account.js
@@ -2,7 +2,7 @@ var http = require('http');
 var httpClient = new http.Client();
 var httpServerHost = "http://tunnel.fibos.io/1.0/app/token/create";
 var account = "fibosphoenix"
-var pubkey = "FO6MNoGkrfAnU7a76G6rjV1MwQsrFJKHeqr2bUUii8ymsCqr3bsK"
+var pubkey = "FO..." //YOUR PUB KEY
 var rep = httpClient.post(httpServerHost, {
                         json: {
                                         account: account,
