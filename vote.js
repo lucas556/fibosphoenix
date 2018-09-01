5212e21115d33457c8c92d319df35286ecb07d18
lucas556 <56923561@qq.com>
2018-09-01 16:00:38 +0800

    Update vote.js

--- vote.js
+++ vote.js
@@ -1,15 +1,17 @@
+var FIBOS = require("fibos.js");
+
 var FIBOS = require('fibos.js');
-// 替换privatekey 为你的私钥
+// 替换xxxprivatekey 为你的私钥
 var fibos = FIBOS({
     chainId: "6aa7bd33b6b45192465afa3553dedb531acaaff8928cf64b70bd4c5e49b7ec6a",
-    keyProvider: "5...", 
-    httpEndpoint: "http://se-rpc.fibos.io:8870",
+    keyProvider: "5K2aRThhojBC25jSMJoydM4hnjFkwn35EcuhHx4WFdowpG3D7QP",
+    httpEndpoint: "http://127.0.0.1:8888",
     logger: {
         log: null,
         error: null
     }
 });
 var ctx = fibos.contractSync("eosio");
-// 替换 你的账号名
-var rs = ctx.voteproducerSync("你的账户名", "", ["fibosphoenix"]);
+// 替换xxxaccount 为你的账号名
+var rs = ctx.voteproducerSync("shanglongjie", "", ["eosteaeostea","fibosisfunoo","fibosphoenix"]);
 console.log(rs);
